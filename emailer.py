64e8169b9c56078bbcb0982da4fc7b38f08d059a
Brad Chamberlain <bradcray@users.noreply.github.com>
2020-09-03 11:32:44 -0700

    Did I mention I don't know Python?

--- emailer.py
+++ emailer.py
@@ -199,7 +199,7 @@ def _get_subject(repo, message):
     else:
         subject_msg = message_lines[0]
     subject_msg = subject_msg[:50]
-    subject = '[Chapel Merge] {1}'.format(repo, subject_msg)
+    subject = '[Chapel Merge] {0}'.format(subject_msg)
     return subject
 
 
